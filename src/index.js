235694f65bcc00ef97ceb9786c3e79475df1aade
Rifure <rifure_k@hotmail.com>
2022-07-19 23:21:57 -0400

    commit 1

--- src/index.js
+++ src/index.js
@@ -4,10 +4,59 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+class Titulo extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+  }
+
+  render() {
+    return(
+      <div>{this.props.title}</div>
+    )
+  }
+
+}
+
+class Form extends React.Component {
+
+  render() {
+    return(
+      <div>
+        <input type={'text'} className="" />
+        <input type={'text'} className="" />
+        <input type={'checkbox'} />
+        <button>Agregar</button>
+      </div>
+    )
+  }
+
+}
+
+class Notas extends React.Component {
+
+
+
+}
+
+class Main extends React.Component {
+
+  render() {
+    return(
+      <div className=''>
+        <Titulo title="Post It Simulator!" />
+        <Form />
+      </div>
+    )
+  }
+
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Main/>
   </React.StrictMode>
 );
 
